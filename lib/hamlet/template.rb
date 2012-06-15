5d3b1c307121d3463e9c3bcf767785243cee2ad8
grosser <grosser.michael@gmail.com>
2012-06-15 09:07:37 -0700

    do not blow up when required in an rails2 app

--- lib/hamlet/template.rb
+++ lib/hamlet/template.rb
@@ -6,7 +6,8 @@ module Hamlet
   if Object.const_defined?(:Rails)
     # Rails template implementation for Hamlet
     # @api public
-    RailsTemplate = Temple::Templates::Rails(Hamlet::Engine,
+    begin
+      RailsTemplate = Temple::Templates::Rails(Hamlet::Engine,
                                              :register_as => :hamlet,
                                              # Use rails-specific generator. This is necessary
                                              # to support block capturing and streaming.
@@ -15,5 +16,8 @@ module Hamlet
                                              # Rails takes care of the capturing by itself.
                                              :disable_capture => true,
                                              :streaming => Object.const_defined?(:Fiber))
+    rescue RuntimeError => e
+      warn "Failed to load RailsTemplate #{e.message}"
+    end
   end
 end
